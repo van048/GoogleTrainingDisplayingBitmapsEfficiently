c3293d9a776b4db93743e358f090bf7b0fe07156
dqshare <739362790@qq.com>
2017-01-07 16:29:04 +0800

    Loading Large Bitmaps Efficiently

---
+++ app/src/main/java/cn/ben/googletrainingdisplayingbitmapsefficiently/LoadingBitmapsActivity.java
@@ -0,0 +1,37 @@
+package cn.ben.googletrainingdisplayingbitmapsefficiently;
+
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class LoadingBitmapsActivity extends AppCompatActivity {
+
+    @SuppressWarnings("FieldCanBeLocal")
+    private ImageView mImageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loading_bitmaps);
+        mImageView = (ImageView) findViewById(R.id.iv_display);
+
+        readDimAndType();
+
+        mImageView.setImageBitmap(
+                BitmapUtil.decodeSampledBitmapFromResource(getResources(), R.drawable.myimage, 100, 100));
+    }
+
+    private void readDimAndType() {
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeResource(getResources(), R.drawable.myimage, options);
+        int imageHeight = options.outHeight;
+        int imageWidth = options.outWidth;
+        String imageType = options.outMimeType;
+        Toast.makeText(this, "imageHeight: " + imageHeight
+                + "\nimageWidth: " + imageWidth
+                + "\nimageType: " + imageType, Toast.LENGTH_SHORT).show();
+    }
+}
