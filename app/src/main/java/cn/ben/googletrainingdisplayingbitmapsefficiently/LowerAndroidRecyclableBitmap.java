f1cb97d9683445805714bb01dcf21efbdb06ca7b
dqshare <739362790@qq.com>
2017-01-10 16:07:53 +0800

    Manage Memory on Android 2.3.3 and Lower

---
+++ app/src/main/java/cn/ben/googletrainingdisplayingbitmapsefficiently/LowerAndroidRecyclableBitmap.java
@@ -0,0 +1,65 @@
+package cn.ben.googletrainingdisplayingbitmapsefficiently;
+
+import android.graphics.Bitmap;
+
+@SuppressWarnings("WeakerAccess")
+public class LowerAndroidRecyclableBitmap {
+    private Bitmap mBitmap = null;
+    private int mCacheRefCount = 0;
+    private int mDisplayRefCount = 0;
+    private boolean mHasBeenDisplayed = false;
+
+    // TODO: 2017/1/10  
+    public LowerAndroidRecyclableBitmap(Bitmap bitmap) {
+        mBitmap = bitmap;
+    }
+
+    // TODO: 2017/1/10 Notify the drawable??
+    // Notify the drawable that the displayed state has changed.
+    // Keep a count to determine when the drawable is no longer displayed.
+    public void setIsDisplayed(boolean isDisplayed) {
+        synchronized (this) {
+            if (isDisplayed) {
+                mDisplayRefCount++;
+                mHasBeenDisplayed = true;
+            } else {
+                mDisplayRefCount--;
+            }
+        }
+        // Check to see if recycle() can be called.
+        checkState();
+    }
+
+    // Notify the drawable that the cache state has changed.
+    // Keep a count to determine when the drawable is no longer being cached.
+    public void setIsCached(boolean isCached) {
+        synchronized (this) {
+            if (isCached) {
+                mCacheRefCount++;
+            } else {
+                mCacheRefCount--;
+            }
+        }
+        // Check to see if recycle() can be called.
+        checkState();
+    }
+
+    private synchronized void checkState() {
+        // If the drawable cache and display ref counts = 0, and this drawable
+        // has been displayed, then recycle.
+        if (mCacheRefCount <= 0 && mDisplayRefCount <= 0 && mHasBeenDisplayed
+                && hasValidBitmap()) {
+            getBitmap().recycle();
+        }
+    }
+
+    private synchronized boolean hasValidBitmap() {
+        Bitmap bitmap = getBitmap();
+        return bitmap != null && !bitmap.isRecycled();
+    }
+
+    private Bitmap getBitmap() {
+        return mBitmap;
+    }
+
+}
